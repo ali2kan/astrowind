6657b04b69171d349155ab7975fa48429504b370
ali2kan <60204416+ali2kan@users.noreply.github.com>
2023-04-06 16:23:53 -0700

    fdsafsd

--- src/config.mjs
+++ src/config.mjs
@@ -3,7 +3,7 @@ import defaultImage from './assets/images/default.png';
 const CONFIG = {
   name: 'AstroWind',
 
-  origin: 'https://astrowind.vercel.app',
+  origin: 'https://test.bravebird.me',
   basePathname: '/',
   trailingSlash: false,
 
