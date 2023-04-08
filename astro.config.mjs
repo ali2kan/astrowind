666ff5afaaf8c900a4f83d8d983b5d3307aecad2
ali2kan <60204416+ali2kan@users.noreply.github.com>
2023-04-07 18:46:53 -0700

    fadsdffds

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,10 @@ import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/config.mjs';
-
+import cloudflare from "@astrojs/cloudflare";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const whenExternalScripts = (items = []) => SITE.googleAnalyticsId ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -48,5 +49,6 @@ export default defineConfig({
         '~': path.resolve(__dirname, './src')
       }
     }
-  }
+  },
+  adapter: cloudflare()
 });
